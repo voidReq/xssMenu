5c5130f1998a48c674a7e45d4dd03fc215129552
voidReq <100312344+voidReq@users.noreply.github.com>
2024-04-16 16:22:25 -0500

    One input

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         button
+// @name         xssMenu
 // @namespace    hi
 // @version      0.31
 // @description  Add sci-hub button on article page. Add sci-hub button after article link. Support Google scholar, bing academic and baidu xueshu. Jump CNKI English article to Chinese article.
@@ -7,26 +7,35 @@
 // @include      *
 // ==/UserScript==
 
-//https://www.htmldog.com/guides/javascript/advanced/creatingelements/
-//btn.style = "position:fixed; top:" + buttonY + "px; left:" + buttonX + "px; background-color: white; border: none; color: #8e8e8e; padding: 0px 0px; text-align: center; text-decoration: none; font-family: ; display: inline-block; font-size: 20px; margin: 0px 0px; cursor: pointer; width: 30px; height: 30px";
-
-/*
-select.addEventListener('mouseover', function() {
-    alert(1);
-});
-*/
+var inputs = document.getElementsByTagName('input');
+var textBox = document.getElementsByName("p1")[0]
+var textBoxRect = textBox.getBoundingClientRect();
 
 
 
 var select = document.createElement("select");
 select.id = "mySelect";
-var payloads = ["<script>alert(1)</script>","<img src='#' onerror=alert(1) />","<b onmouseover=alert('Wufff!')>click me!</b>"];
 
-var select_width = 130;
+
+
+
+
+
+var payloads = [" ", "<script>alert(document.domain)</script>","<img src='#' onerror=alert(document.domain) />","<b onmouseover=alert(document.domain)>click me!</b>"];
+
+
+var select_width = 200;
 var select_height = 40;
-var selectX = document.documentElement.clientWidth - 400;
-var selectY = document.documentElement.clientHeight - 400;
+var selectX = textBoxRect.right + 65; // Adjust 10 pixels to the right of the text box
+var selectY = textBoxRect.top - 10;
 
+select.style = "position:absolute; top:" + selectY + "px; left:" + selectX + "px; background-color: #00ccff; border-radius: 10px; font-size: 20px; width:" + select_width + "px; height:" + select_height + "px";
+select.style.opacity = "0.9";
+
+
+select.onchange = function () {
+    document.getElementsByName("p1")[0].value = select.value
+};
 
 for (var i = 0; i < payloads.length; i++) {
     var option = document.createElement("option");
@@ -36,14 +45,4 @@ for (var i = 0; i < payloads.length; i++) {
 }
 
 
-select.onchange = function () {
-    document.getElementsByName("p1")[0].value = select.value
-};
-
-
-
-select.style = "position:fixed; top:" + selectY + "px; left:" + selectX + "px; background-color: #00ccff; border-radius: 20px; font-size: 20px; cursor: pointer; width:" + select_width + "px; height:" + select_height + "px";
-select.style.opacity = "0.9";
-
-
-document.body.appendChild(select);
+textBox.parentNode.appendChild(select);
