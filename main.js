a8fba05ba405c9e3d6ec6fc6cba1c245ab42b7c3
voidReq <100312344+voidReq@users.noreply.github.com>
2024-04-16 16:42:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,33 +8,39 @@
 // ==/UserScript==
 
 var inputs = document.getElementsByTagName('input');
-//var textBox = document.getElementsByName("p1")[0]
-
 var payloads = [" ", "<script>alert(document.domain)</script>","<img src='#' onerror=alert(document.domain) />","<b onmouseover=alert(document.domain)>click me!</b>"];
 var select_width = 200;
 var select_height = 40;
 
-
 for (let i = 0; i < inputs.length; i++) {
-   var thisInput = inputs[i];
-   var select = document.createElement("select"+i);
-   select.id = "mySelect";
-   var inputRect = thisInput.getBoundingClientRect();
-   var selectX = inputRect.right + 65;
-   var selectY = inputRect.top - 10;
-   select.style = "position:absolute; top:" + selectY + "px; left:" + selectX + "px; background-color: #00ccff; border-radius: 10px; font-size: 20px; width:" + select_width + "px; height:" + select_height + "px";
-   select.style.opacity = "0.9";
-        select.onchange = function () {
-        document.getElementsByName("p1")[0].value = select.value
-   };
-   for (var a = 0; a < payloads.length; a++) {
-       var option = document.createElement("option");
-       option.value = payloads[a];
-       option.text = payloads[a];
-       select.appendChild(option);
-   }
-   inputs[i].parentNode.appendChild(select);
+    if(inputs[i].type != "text"){
+        delete inputs[i]
+    }
 }
 
 
+function x(i, curVal) {
+    document.getElementById("mySelect" + i).value = curVal;
+}
 
+for (let i = 0; i < inputs.length; i++) {
+    var thisInput = inputs[i];
+    var select = document.createElement("select");
+    select.id = "mySelect" + i;
+    var inputRect = thisInput.getBoundingClientRect();
+    var selectX = inputRect.right + 65;
+    var selectY = inputRect.top - 10;
+    select.style = "position:absolute; top:" + selectY + "px; left:" + selectX + "px; background-color: #00ccff; border-radius: 10px; font-size: 20px; width:" + select_width + "px; height:" + select_height + "px";
+    select.style.opacity = "0.9";
+    for (let a = 0; a < payloads.length; a++) {
+        var option = document.createElement("option");
+        option.value = payloads[a];
+        option.text = payloads[a];
+        select.appendChild(option);
+    }
+    var curVal = select.value;
+    select.onchange = function () {
+        x(i, select.value);
+    };
+    inputs[i].parentNode.appendChild(select);
+}
